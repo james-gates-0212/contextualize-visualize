401ebb0efd3c0eada3c240da63f87c036268e44d
james-gates-0212 <james.gates.0212@gmail.com>
2022-07-13 16:12:14 +0900

    chore: fix remove unnecessary import

--- src/plots/index.ts
+++ src/plots/index.ts
@@ -3,7 +3,6 @@ import HistogramPlot from "./histogram";
 import LinePlot from "./line";
 
 export { LinePlot, HistogramPlot, GraphPlot };
-export * from "./tree";
 export * from "./graph";
 export * from "./scatter";
 export * from "./histogram";
